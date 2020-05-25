e01e0c6a18f06c4fae1e43abea712623c56a8257
AshitaGK <ashita.gk696@gmail.com>
2020-05-25 23:44:36 +0530

    Updates

---
+++ precache-manifest.55960b2393436aa2f20da9b98efd1e68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672fb07320209ebb471839e7f446cf01",
+    "url": "/React-Form/index.html"
+  },
+  {
+    "revision": "6c3d1bf1d7a4c94bd204",
+    "url": "/React-Form/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cd03f29ba45c27412ed1",
+    "url": "/React-Form/static/css/main.5c54e915.chunk.css"
+  },
+  {
+    "revision": "6c3d1bf1d7a4c94bd204",
+    "url": "/React-Form/static/js/2.4c5ee97e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Form/static/js/2.4c5ee97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd03f29ba45c27412ed1",
+    "url": "/React-Form/static/js/main.fa2b8a86.chunk.js"
+  },
+  {
+    "revision": "267a03aef49be32669a6",
+    "url": "/React-Form/static/js/runtime-main.6b3e443f.js"
+  }
+]);
